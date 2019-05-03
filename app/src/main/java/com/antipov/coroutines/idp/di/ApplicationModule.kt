f2fd0140dd0d11a1bc7924cb9d7ca32b3124e066
Alexander Antipov <antipov@themindstudios.com>
2019-05-03 08:50:07 +0300

    added coroutines to project

--- app/src/main/java/com/antipov/coroutines/idp/di/ApplicationModule.kt
+++ app/src/main/java/com/antipov/coroutines/idp/di/ApplicationModule.kt
@@ -1,15 +1,6 @@
 package com.antipov.coroutines.idp.di
 
-import android.app.Application
-import com.antipov.coroutines.idp.TestDependency
 import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
 
 @Module
-class ApplicationModule {
-
-    @Provides
-    @Singleton
-    fun provideTestDependency(context: Application) = TestDependency()
-}
+class ApplicationModule
--- app/src/main/java/com/antipov/coroutines/idp/ui/main/MainActivity.kt
+++ app/src/main/java/com/antipov/coroutines/idp/ui/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.antipov.coroutines.idp.ui.main
 
+import android.os.Bundle
 import com.antipov.coroutines.idp.R
 import com.antipov.coroutines.idp.navigation.AppNavigator
 import com.antipov.coroutines.idp.ui.base.BaseActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : BaseActivity(), MainView {
@@ -23,7 +27,12 @@ class MainActivity : BaseActivity(), MainView {
 
     override fun getActivityNavigator() = navigator
 
-    override fun onResume() {
-        super.onResume()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GlobalScope.launch {
+            delay(1000)
+            println("Kotlin Coroutines World in launch")
+        }
+        println("Kotlin Coroutines World!")
     }
 }
